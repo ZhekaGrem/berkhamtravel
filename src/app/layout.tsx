4834bd14c2236ca92f53db08f12ccb7a489a5377
Jeka <odinked@gmail.com>
2026-01-21 14:39:30 +0200

    add clarity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -236,7 +236,15 @@ export default function RootLayout({
           strategy="afterInteractive"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(structuredData) }}
         />
-<GoogleAnalytics gaId="G-1TL6LPN01T" />
+        <Script type="text/javascript" id="microsoft-clarity" strategy="lazyOnload">
+          {`(function(c,l,a,r,i,t,y){
+            c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments) };
+          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "v4wxwqa8n0");`}
+        </Script>
+        <GoogleAnalytics gaId="G-1TL6LPN01T" />
+
       </head>
       <body className={`${playfairDisplay.variable} ${montserrat.variable} ${cormorant.variable}`}>
         {children}
