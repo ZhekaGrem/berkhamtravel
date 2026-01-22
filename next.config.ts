af80c3b13d953e2068a31f47817264bfa5e3462a
Jeka <odinked@gmail.com>
2026-01-22 16:53:23 +0200

    unoptimized true

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,9 @@ const nextConfig: NextConfig = {
     deviceSizes: [375, 640, 768, 1024, 1280, 1440, 1920],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     minimumCacheTTL: 60 * 60 * 24 * 30, // 30 days cache
+    unoptimized: true,
   },
+  staticPageGenerationTimeout: 520,
   experimental: {
     optimizeCss: true,
   },
